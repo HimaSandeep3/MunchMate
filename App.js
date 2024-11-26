13c2cac585fac5cb5718e8791306ce67af37fefe
Hima Sandeep Vartha <Sandeep.Vartha@dhanvita.com>
2024-11-26 20:42:51 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement('div', { className: 'grandParent' }, 
         React.createElement('div', { className: 'parent' }, 
-        [React.createElement('h1', { className: 'child' },'H1 Tag'),
+        [React.createElement('h1', { className: 'child' },'H1 Tag- Header'),
         React.createElement('p', {},'This is a paragraph')]));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
